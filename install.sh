77591918096f1f9b5206e8bae2fc8eccf48e3f92
Glen Campbell <glen@broadpool.com>
2013-02-11 09:58:32 -0600

    not needed

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rsync -aprv index.html css img js \
-	root@rack2.broadpool.net:/var/www/vhosts/xlerb.com/
-ssh root@rack2.broadpool.net 'chown -R apache:apache /var/www/vhosts/xlerb.com'
