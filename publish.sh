f3067b78aafa460fe971b2535270ecd7a2c4212c
Glen Campbell <glen@broadpool.com>
2015-09-15 16:04:45 -0700

    cache-control

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 if [ -d site ]; then
     cd site
-    aws s3 sync . s3://xlerb.com
+    aws s3 sync . s3://xlerb.com --cache-control "max-age=600"
 else
     echo "You're not in the right directory"
     exit
